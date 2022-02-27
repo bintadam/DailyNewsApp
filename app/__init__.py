f8b1dae2b90eec7f460b37a53e5d0892331f849e
zakiya adam <zakiyaadan12@gmail.com>
2022-02-27 16:36:23 +0300

    imported DevConfig in my __init__.py class

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from Config import DevConfig
 
 # Initializing application
 app = Flask(__name__)
+
+# Setting up configuration
+app.config.from_object(DevConfig)
 
 from app import views
