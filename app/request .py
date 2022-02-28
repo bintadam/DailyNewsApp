fdfb05a0bbeff310c44f9fb9c8283dc8f0caa4de
zakiya adam <zakiyaadan12@gmail.com>
2022-02-28 16:51:07 +0300

    added method process_articles

--- app/request .py
+++ app/request .py
@@ -8,3 +8,20 @@ def get_articles(source):
     content = request.urlopen(path)
     jason = jason.loads(content.read())
     return jason ['articles']
+
+
+def process_articles(articles):
+    my_articles = []
+    for each in articles:
+        a = article(
+            each['source'],
+            each['title'],
+            each['author'],
+            each['urlToImage'],
+            each['url'],
+            each['publishedAt'],
+            each['description'],
+            each['content'],   
+        )
+        my_articles.append(a)
+        return my_articles
