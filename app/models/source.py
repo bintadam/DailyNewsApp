ecf22a45f2a8db91d8f3d0b15dc7e108869a9f02
zakiya adam <zakiyaadan12@gmail.com>
2022-02-28 15:18:44 +0300

    created class source and added  __init__ method

---
+++ app/models/source.py
@@ -0,0 +1,12 @@
+class source:
+    '''
+    source class to define source 
+    '''
+
+    def __init__(self,id,name,description, country,url,language):
+        self.id =id
+        self.name = name
+        self.description = description
+        self.country = country
+        self.url = url
+        self.language = language
