4782ef480205f3af226387c0f9aeafc285ee9d7f
zakiya adam <zakiyaadan12@gmail.com>
2022-02-28 21:19:29 +0300

    imported os

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
-    MY_API_KEY = 
+    MY_API_KEY = os.environ.get('MY_API_KEY')
+
     MY_NEWS_BASE_URL ='https://newsapi.org'
     ARTICLES ='https://newsapi.org/v2/everything?sources={}&apikey=' + str(MY_API_KEY)
     SOURCES = 'https://newsapi.org/v2/top-headlines/sources?category={}apikey=' + str(MY_API_KEY)
@@ -13,23 +16,12 @@ class Config:
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
+    
     pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
+   
     DEBUG = True
 
 
